40c6cc66e79613ea1a401cc598f62cac6499a4c9
novagurl <sherese.smith@mycavehill.uwi.edu>
2018-09-19 20:06:17 -0400

    data.h

--- data.h
+++ data.h
@@ -6,15 +6,15 @@ int x;
 float y;
 
 public:
-    data ()
-void set y (float a);
+    data ();
+
 data (int a, float b);
 
-int get x ();
-float get y ();
+int get x();
+float get y();
 
 void set x (int a);
+void set y(float b);
 
 };
-
 #endif
